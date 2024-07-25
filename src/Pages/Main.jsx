84256ddceae78c7c4d4f1878718a913822fff8bb
Robinson Chalas <robinsonchalasj@gmail.com>
2024-07-25 15:56:22 -0700

    some updates

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { Outlet, Link, useNavigate } from 'react-router-dom';
 
 const Main = () => {
   const navigate = useNavigate();
 
+  useLayoutEffect(() => {
+    const mainContent = document.querySelector('.main-content');
+    if (mainContent) {
+      mainContent.style.opacity = 1;
+    }
+  }, []);
 
   return (
     <div className="d-flex">
       <nav className="sidebar bg-light">
         <h2>Dashboard</h2>
         <ul>
-        <li>
+          <li>
             <Link to="admin">Admin Dashboard</Link>
           </li>
           <li>
@@ -28,13 +34,12 @@ const Main = () => {
         </ul>
         
         <div className="d-flex justify-content-center align-items-center" style={{ height: '90vh' }}>
-        <button className="btn btn-info text-light position-relative rounded-pill"style={{ width: '140vh' }}>
-          Log Out
-        </button>
+          <button className="btn btn-info text-light position-relative rounded-pill" style={{ width: '140px' }}>
+            Log Out
+          </button>
         </div>
       </nav>
-      <div className="main-content p-3" style={{ marginLeft: '250px', width: 'calc(100% - 250px)' }}>
-        {/* <h1>Main Page</h1> */}
+      <div className="main-content p-3" style={{ marginLeft: '250px', width: 'calc(100% - 250px)', opacity: 0 }}>
         <Outlet />
       </div>
     </div>
